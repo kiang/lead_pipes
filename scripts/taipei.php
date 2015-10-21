3e82e148e85f1e6a08bdcd469944566383145735
kiang <kiange@gmail.com>
2015-10-22 02:23:15 +0800

    generate taipei.json

--- scripts/taipei.php
+++ scripts/taipei.php
@@ -1,17 +1,31 @@
 <?php
 
 /*
+ * ref http://logbot.g0v.tw/channel/g0v.tw/2015-10-21
+ * raw https://gist.github.com/audreyt/435dd6f7996ea7547f41
+ * 
+ * require php5-geos (Ubuntu 15.04)
  * 
  */
 $basePath = dirname(__DIR__);
+include_once($basePath . '/libs/geoPHP/geoPHP.inc');
 
 $tmpPath = $basePath . '/tmp';
 if (!file_exists($tmpPath)) {
     mkdir($tmpPath, 0777, true);
 }
+/*
+ * ref http://wiki.openstreetmap.org/wiki/Nominatim
+ */
 $baseUrl = 'http://nominatim.openstreetmap.org/search?format=json&county=taipei&country=taiwan';
+/*
+ * ref http://wiki.openstreetmap.org/wiki/API_v0.6
+ */
 $apiUrl = 'https://www.openstreetmap.org/api/0.6/way';
-$p = xml_parser_create();
+
+$fc = new stdClass();
+$fc->type = 'FeatureCollection';
+$fc->features = array();
 
 $fh = fopen($basePath . '/data/taipei.csv', 'r');
 fgetcsv($fh, 2048);
@@ -19,6 +33,7 @@ while ($line = fgetcsv($fh, 2048)) {
     if (empty($line[1])) {
         continue;
     }
+    $line1 = $line[1];
     $line[1] = substr($line[1], 9);
     $pos = strpos($line[1], '區') + 3;
     $city = substr($line[1], 0, $pos);
@@ -34,6 +49,7 @@ while ($line = fgetcsv($fh, 2048)) {
     }
     echo "{$tmpFile}\n";
     $results = json_decode(file_get_contents($tmpFile), true);
+    $osmLines = array();
     foreach ($results AS $node) {
         if ($node['osm_type'] === 'way') {
             $tmpXml = $path . '/' . $node['osm_id'] . '.xml';
@@ -41,10 +57,39 @@ while ($line = fgetcsv($fh, 2048)) {
                 echo "getting {$node['osm_id']}\n";
                 file_put_contents($tmpXml, file_get_contents($apiUrl . "/{$node['osm_id']}/full"));
             }
-            continue;
+            $p = xml_parser_create();
             xml_parse_into_struct($p, file_get_contents($tmpXml), $vals);
-            print_r($vals);
-            exit();
+            xml_parser_free($p);
+            $points = array();
+            foreach ($vals AS $val) {
+                if (isset($val['attributes']['LAT'])) {
+                    $points[] = new Point($val['attributes']['LON'], $val['attributes']['LAT']);
+                }
+            }
+            $osmLines[] = new LineString($points);
         }
     }
+    if (!empty($osmLines)) {
+        $unionLine = false;
+        foreach ($osmLines AS $osmLine) {
+            if (false === $unionLine) {
+                $unionLine = $osmLine;
+            } else {
+                $unionLine->union($osmLine);
+            }
+        }
+
+        $f = new stdClass();
+        $f->type = 'Feature';
+        $f->geometry = json_decode($unionLine->out('json'));
+        $f->properties = array(
+            'title' => $line1,
+            'meters' => intval($line[2]),
+            'bolts' => intval($line[3]),
+        );
+        $fc->features[] = $f;
+    }
 }
+fclose($fh);
+
+file_put_contents($basePath . '/data/taipei.json', json_encode($fc, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
