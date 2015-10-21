86efb1e4fa343813cb254b71c7a4b20aca576e93
kiang <kiange@gmail.com>
2015-10-22 01:18:24 +0800

    getting osm way xml

--- scripts/taipei.php
+++ scripts/taipei.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * 
  */
@@ -9,12 +10,13 @@ if (!file_exists($tmpPath)) {
     mkdir($tmpPath, 0777, true);
 }
 $baseUrl = 'http://nominatim.openstreetmap.org/search?format=json&county=taipei&country=taiwan';
-$apiUrl = 'https://www.openstreetmap.org/api/0.6/way/{id}/full';
+$apiUrl = 'https://www.openstreetmap.org/api/0.6/way';
+$p = xml_parser_create();
 
 $fh = fopen($basePath . '/data/taipei.csv', 'r');
 fgetcsv($fh, 2048);
 while ($line = fgetcsv($fh, 2048)) {
-    if(empty($line[1])) {
+    if (empty($line[1])) {
         continue;
     }
     $line[1] = substr($line[1], 9);
@@ -30,5 +32,19 @@ while ($line = fgetcsv($fh, 2048)) {
     if (!file_exists($tmpFile)) {
         file_put_contents($tmpFile, file_get_contents($url));
     }
+    echo "{$tmpFile}\n";
     $results = json_decode(file_get_contents($tmpFile), true);
+    foreach ($results AS $node) {
+        if ($node['osm_type'] === 'way') {
+            $tmpXml = $path . '/' . $node['osm_id'] . '.xml';
+            if (!file_exists($tmpXml)) {
+                echo "getting {$node['osm_id']}\n";
+                file_put_contents($tmpXml, file_get_contents($apiUrl . "/{$node['osm_id']}/full"));
+            }
+            continue;
+            xml_parse_into_struct($p, file_get_contents($tmpXml), $vals);
+            print_r($vals);
+            exit();
+        }
+    }
 }
